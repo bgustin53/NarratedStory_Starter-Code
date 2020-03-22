e5502c1b42f08a3b61cfe750b62a4c02106ce999
Bruce Gustin <bgustin@srcs.org>
2020-03-21 20:04:29 -0700

    Updated Starter Code

---
+++ Counter.java
@@ -0,0 +1,51 @@
+import greenfoot.*;
+
+/**
+ * A Counter class that allows you to display a numerical value on screen.
+ * 
+ * The Counter is an actor, so you will need to create it, and then add it to
+ * the world in Greenfoot.  If you keep a reference to the Counter then you
+ * can adjust its value.
+ * 
+ * @author Neil Brown and Michael Kölling 
+ * @version 1.1
+ */
+public class Counter extends Actor
+{
+    private static final Color transparent = new Color(0,0,0,0);
+    private GreenfootImage background;
+    private String prefix;
+    
+    public Counter()
+    {
+        this(new String());
+    }
+
+    /**
+     * Create a new counter, initialised to 0.
+     */
+    public Counter(String prefix)
+    {
+        background = getImage();  // get image from class
+        this.prefix = prefix;
+        updateImage();
+    }
+
+    /**
+     * Update the image on screen to show the current value.
+     */
+    private void updateImage()
+    {
+        GreenfootImage image = new GreenfootImage(background);
+        GreenfootImage text = new GreenfootImage(prefix, 22, Color.BLACK, transparent);
+        
+        if (text.getWidth() > image.getWidth() - 20)
+        {
+            image.scale(text.getWidth() + 20, image.getHeight());
+        }
+        
+        image.drawImage(text, (image.getWidth()-text.getWidth())/2, 
+                        (image.getHeight()-text.getHeight())/2);
+        setImage(image);
+    }
+}
